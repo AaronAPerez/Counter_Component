1528236120f5d91cde3094228f6d5a2a2269c668
AaronAPerez <aaperez06@gmail.com>
2024-05-10 17:31:33 -0700

    Added comments to explain code

--- Counter_Component/src/components/Content.tsx
+++ Counter_Component/src/components/Content.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState } from "react"; // Import the useState hook from React
 
 const Content = () => {
-  const [counter, setCounter] = useState(0);
+  // functional component called "Content"
+  const [counter, setCounter] = useState(0); // Initialize the "counter" state with 0 using the useState hook
 
   const minusNum = () => {
+    // function to decrement the counter
     setCounter((minusNum) => (minusNum > 0 ? minusNum - 1 : 0));
-  };
+  }; //// Update the counter state by decreasing it if it's greater than 0, otherwise set it to 0
 
   const plusNum = () => {
-    setCounter((plusNum) => plusNum + 1);
+    // function to increase the counter
+    setCounter((plusNum) => plusNum + 1); // Update the counter state by increasing it
   };
 
   const resetNum = () => {
-    setCounter(0);
+    // function to reset the counter
+    setCounter(0); // Update the counter state by setting it to 0
   };
 
   return (
@@ -22,26 +26,28 @@ const Content = () => {
           <div className="row py-4">
             <div className="col">
               <h1>
-              LEVEL UP
-              <div className="row"></div>
-            {counter}</h1>
+                LEVEL UP
+                <div className="row"></div>
+                {counter}{" "}
+                {/* Display the {counter} text "LEVEL UP" and the current value of the counter */}
+              </h1>
             </div>
           </div>
           <div className="row">
             <div className="col">
               <button className="btn btn-danger" onClick={minusNum}>
-                -
+                - {/* Button to decrease the counter */}
               </button>
 
               <button className="btn btn-success" onClick={plusNum}>
-                +
+                + {/* Button to increase the counter */}
               </button>
             </div>
           </div>
           <div className="row">
             <div className="col">
               <button className="btn btn-warning" onClick={resetNum}>
-                RESET
+                RESET {/* Button to reset the counter */}
               </button>
             </div>
           </div>
@@ -51,4 +57,4 @@ const Content = () => {
   );
 };
 
-export default Content;
+export default Content; // Export the Content component
